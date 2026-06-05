29d6d17e4395c135c46e3741417723df7d65a97f
wangyanan333 <wangyanan7084@163.com>
2026-06-05 18:46:47 +0800

    Update DSMI.m

--- common/DSMI.m
+++ common/DSMI.m
@@ -1,1 +1,22 @@
+function val = DSMI(target_state_Sic, state_Sjd, conditioning_set_S)
+% DSMI: Differential State Mutual Information
+    h_YZ = h_multivariate([state_Sjd, conditioning_set_S]);
+    h_XZ = h_multivariate([target_state_Sic, conditioning_set_S]);
+    h_Z  = h(conditioning_set_S);
+    h_YXZ = h_multivariate([state_Sjd, target_state_Sic, conditioning_set_S]);
+    
+    val = h_YZ + h_XZ - h_Z - h_YXZ;
+end
+function H = h_multivariate(data)
+    [~, ~, uidx] = unique(data, 'rows');
+    p = accumarray(uidx, 1) / size(data, 1);
+    H = -sum(p .* log2(p));
+end
 
+function H = h(data)
+    if isempty(data)
+        H = 0;
+        return;
+    end
+    H = h_multivariate(data);
+end
